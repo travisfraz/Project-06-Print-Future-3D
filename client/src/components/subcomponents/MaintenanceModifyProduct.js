a43767ba5aee7f4372ebdeaef8fd8a547c8155a9
travisfraz <travisfraz@gmail.com>
2020-10-29 08:32:16 -0500

    Prod search logic and init modify JSX

--- client/src/components/subcomponents/MaintenanceModifyProduct.js
+++ client/src/components/subcomponents/MaintenanceModifyProduct.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 
 export default function MaintenanceModifyProduct() {
     const [search, setSearch] = useState('')
-    const [chooseProducts, setChooseProducts] = useState([])
+    const [chooseModifyProd, setChooseModifyProd] = useState([])
 
+    //Finds all products matching the search criteria.
     async function findProducts(e) {
         e.preventDefault()
         //Creates a url that will have the query search parameters added.
@@ -13,19 +14,74 @@ export default function MaintenanceModifyProduct() {
 
         const response = await fetch(url)
         const resJSON = await response.json()
-        const temp = resJSON.map(ele => {
-            return (
-                <div key={ele._id} id={ele._id}>
-                    <label>{ele.name}</label>
-                    <button name='joe' onClick={populateProduct}>Modify</button>
-                </div>
-                
-            )
-        })
-        setChooseProducts(temp)
+
+        const temp = popChooseProd(null, resJSON)
+        setChooseModifyProd(temp)
     }
 
-    function populateProduct(e) {
+    //Populates the modify section with either, "No Products Found", list of products found for selection, or the
+    //product selected/only product found to be modified.
+    function popChooseProd(e, productData) {
+        let compJSX = null
+        if (e) {
+            console.log('Will populate chosen product')
+            return null
+        } else if (productData.length > 1) {
+            compJSX = productData.map(ele => {
+                return (
+                    <div key={ele._id} id={ele._id}>
+                        <label>{ele.name}</label>
+                        <button onClick={popChooseProd}>Modify</button>
+                    </div>
+                )
+            })
+            return compJSX
+        } else if (productData.length < 1) {
+            return <div>No products found</div>
+        } else {
+            console.log('Will populate only product found')
+            console.log(productData)
+            const sProdData = productData[0]
+            return (
+                <form>
+                    <div>
+                        <label>{sProdData.name}</label>
+                        <input type='text' />
+                    </div>
+                    <div>
+                        <label>{sProdData.desc}</label>
+                        <input type='textarea' />
+                    </div>
+                    <div>
+                        <label>{sProdData.price}</label>
+                        <input type='text' />
+                    </div>
+                    <div>
+                        <img src='' alt='' />
+                        <input type='file' />
+                    </div>
+                </form>
+            )
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return null
+        }
+    }
+
+
+
+    function popModifyProd(e) {
         e.preventDefault()
         const selectedEle = e.target.parentElement
         console.log(selectedEle.id)
@@ -43,7 +99,7 @@ export default function MaintenanceModifyProduct() {
                 />
                 <button>Submit</button>
             </form>
-            {chooseProducts}
+            {chooseModifyProd}
         </div>
     )
 }
